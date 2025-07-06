7efac827c2efabd75b2722b1979bdadc134076e1
Jon <jonnsecora@gmail.com>
2025-07-05 23:43:25 -0400

    Lol. Empty rivers file so I can fork this Branch into a Rivers one.

---
+++ SmoothCoastlines/Rivers/GenRivers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmoothCoastlines.Rivers {
+
+    public class GenRivers {
+
+    }
+}
