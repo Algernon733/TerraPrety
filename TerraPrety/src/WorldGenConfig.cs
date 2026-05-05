9e8227a3d2f9d040b2789386a309c4a139dfb0ad
Darkluke1111 <lukas.gromer@gmx.de>
2026-05-05 12:27:22 +0200

    update configs to v7.10.0

--- TerraPrety/src/WorldGenConfig.cs
+++ TerraPrety/src/WorldGenConfig.cs
@@ -8,9 +8,9 @@
 
         public bool Delicate_configs_below__alter_at_your_own_peril = false;
 
-        public float oceanWobbleScale = 1.5f;
-        public float oceanWobbleIntensity = 0.75f;
-        public double[] remappingKeys = { 0.125, 0.45 };
+        public float oceanWobbleScale = 2.0f;
+        public float oceanWobbleIntensity = 1.0f;
+        public double[] remappingKeys = { 0.115, 0.285 };
         public double[] remappingValues = {0.0, 1.0 };
 
         public int heightMapOctaves = 1;
@@ -18,15 +18,15 @@
 
         public double[] midHeightKeys = { 0.0, 0.05, 0.33333, 1.0 };
         public double[] midHeightValues = { 1.0, 0.9, 0.0, 0.0 };
-        public float chanceForMidZone = 0.2f;
-        public float targetMidLevel = 0.5f;
-        public float lowThreshForMidZone = 0.35f;
+        public float chanceForMidZone = 1.0f;
+        public float targetMidLevel = 0.2f;
+        public float lowThreshForMidZone = 0.2f;
 
         public float radiusMultOutwardsForSmoothing = 6.0f;
 
-        public float[] heightThresholdsForOceanicityComp = { 0.2f, 0.5f, 0.7f, 0.8f, 1.0f };
-        public float[] heightMultsAtThresholdsForOceanicityComp = { 0.0f, 70.0f, 70.0f, 115.0f, 115.0f };
-        public float[] heightFlatsAtThresholdsForOceanicityComp = { 4.2f, 2f, 2f, 4f, 4f };
+        public float[] heightThresholdsForOceanicityComp = { 0.0f, 1.0f };
+        public float[] heightMultsAtThresholdsForOceanicityComp = { 0.0f, 0.0f };
+        public float[] heightFlatsAtThresholdsForOceanicityComp = { 0.0f, 0.0f };
         //public float heightAboveWhichToWatchOceanicity = 0.8f;
         //public float highHeightLowOceanicityMin = 6.15f;
         //public float highHeightLowOceanicityMax = 24.6f;
